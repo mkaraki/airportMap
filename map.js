24f0e03e5a9299325248aea8ab0d29cf7274242c
mkaraki <26180919+mkaraki@users.noreply.github.com>
2023-06-06 01:13:11 +0900

    Change map to photo

--- map.js
+++ map.js
@@ -7,8 +7,16 @@ let defaultMapState = JSON.parse(localStorage.getItem('lastMapState') ?? '[{"lat
 let lastZoom = -1, lastPos = {};
 const map = L.map('map').setView(defaultMapState[0], defaultMapState[1]);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
+
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
 }).addTo(map);
 
 const lc = L.control.locate().addTo(map);
